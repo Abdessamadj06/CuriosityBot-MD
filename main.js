d07fbdcb07eadf679377f19f460767b2090e0091
Majnon rossi <113805445+Abdessamadj06@users.noreply.github.com>
2024-03-18 01:40:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -290,7 +290,7 @@ console.log(chalk.yellow('⚠️ㅤEscanea este codigo QR, el codigo QR expira e
 if (connection == 'open') {
 console.log(chalk.yellowBright('\n╭───────────────────────────◉\n│\n│Conectado correctamente al WhatsApp.\n│\n╰───────────────────────────◉\n'))}
 //if (conn.user.connect) {
-//conn.fakeReply('5217294888993@s.whatsapp.net', '😃', '0@s.whatsapp.net', '😅 Soy CuriosityBot\nRecientemente me e conectado', '0@s.whatsapp.net')
+//conn.fakeReply('212641207087@s.whatsapp.net', '😃', '0@s.whatsapp.net', '😅 Soy CuriosityBot\nRecientemente me e conectado', '0@s.whatsapp.net')
 //conn.user.connect = true;
 //}
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
